ba2cdeabb0d468c25e97bf9f293fb34e4bb6058b
Mujo Didic <80248965+MujoDidic@users.noreply.github.com>
2022-11-26 22:46:59 +0100

    Finished

--- js/site.js
+++ js/site.js
@@ -1,4 +1,44 @@
-//Call Hello World
-function helloWorld(){
-    alert("Hello World");
+// Get string
+//controller function
+function getStringInput(){
+    
+    document.getElementById("alert").classList.add("invisible");
+    
+    let inputString = document.getElementById("inputString").value;
+    
+    let revInputString = reverseString(inputString);
+    
+    displayString(revInputString);
+
+
+}
+//Reverse string
+//Logic function
+function reverseString(inputString){
+
+    let revString = [];
+
+    //reverse a string using a for loop
+
+    for (let index = inputString.length - 1; index >= 0; index--) {
+        revString += inputString[index];
+        
+    }
+
+    return revString;
+    
+}
+
+
+//Display message/string
+//View function
+function displayString(revInputString){
+
+//write message to he screen
+    document.getElementById("msg").innerHTML = `Your reversed string is: ${revInputString}`;
+
+//turn on alert box
+    document.getElementById("alert").classList.remove("invisible");
+
+
 }
